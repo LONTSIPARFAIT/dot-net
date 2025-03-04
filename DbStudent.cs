7491bf2a77739b04154cc7521b5dfff812ea154d
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-04 15:56:27 +0100

    First commit

--- DbStudent.cs
+++ DbStudent.cs
@@ -11,15 +11,32 @@ namespace CRUD_DotNet
     {
         public static MySqlConnection GetMySqlConnection()
         {
-            String connectionString = "server=localhost;" +
-                                  "database=student-dot-net;" +
-                                  "username=root;" +
-                                  "pwd=; port=3306;";
+            String connectionString = "server=localhost;database=student-dot-net;username=root;pwd=;port=3306;";
             MySqlConnection connect = new MySqlConnection(connectionString);
             connect.Open();
+            //Console.WriteLine("Connected success");
+            //connect.Close;
+            return connect;
+        }
 
-            Console.WriteLine("Connected success");
-            connect.Close();
+        public static AddStudent(Student student)
+        {
+            MySqlConnection connect = GetMySqlConnection();
+
+            // Definir la requette SQL d'insertion
+            string query = "INSERT INTO student (name, resister, class, section) VALUES (@name, @resister, @class, @section)";
+            MySqlCommand command = new MySqlCommand(query, connect);
+
+            command.Parameters.AddWithValue("@name", student.Name);
+            command.Parameters.AddWithValue("@register", student.Register);
+            command.Parameters.AddWithValue("@class", student.Class);
+            command.Parameters.AddWithValue("@section", student.Section);
+
+            //Exeption de la requete
+            command.ExecuteNonQuery();
+
+            //Afficher un message de confirmation
+            MessageBox
         }
     }
 }
